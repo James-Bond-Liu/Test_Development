f72c4cd947bf16be3db1e47666fa6450a1a28f2d
liufei <779542742@qq.com>
2021-12-14 20:42:39 +0800

    进程demo1

--- 性能/run.py
+++ 性能/run.py
@@ -1,14 +1,24 @@
 from multiprocessing import Process
 import requests
+import time
 
-class Run(Process):
-    def __init__(self, data):
-        super().__init__()
-        self.data = data
+def run():
+    res = requests.request(method='GET', url='https://www.baidu.com/')
+    print(res.elapsed.total_seconds())
 
-    def run(self):
-        res = requests.request(method='**', url='**', json='**')
-        res.elapsed.total_seconds()
+
+if __name__ == '__main__':
+    start_time = 20
+    while True:
+        for i in range(1000):
+            p = Process(target=run)
+            p.start()
+            end_time = 30
+            time = end_time-start_time
+            if time == 30:  # 持续发起请求30分钟
+                break
+
+
 
 
 
